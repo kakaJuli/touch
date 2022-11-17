268e8abd546d7324c6988db2a4e0e0411cb62ee1
DESKTOP-AHTMLK3\sotni <sotnikov8383@gmail.com>
2022-11-17 18:41:21 +0300

    Vossozdal zanovo

---
+++ src/components/LeftMenu/LeftMenu.js
@@ -0,0 +1,39 @@
+import "./LeftMenu.css";
+
+export const LeftMenu = () => {
+  return (
+    <div className="LeftMenu">
+    <div className="Conteiner">
+      <div className="Logo"></div>
+      <div className="TextLogo">Teach.</div>
+      <div className="Avatar"></div>
+      <div className="TextAvatar">Jone Cooper</div>
+      <div className="Line"></div>
+      <div className="DashboardConteiner">
+      <div className="DashboardLogo"></div>
+      <div className="DashboardText">Dashboard</div>
+      </div>
+      <div className="ChatConteiner">
+      <div className="ChatLogo"></div>
+      <div className="ChatText">Chat</div>
+      </div>
+      <div className="MyCourseConteiner">
+      <div className="MyCourseLogo"></div>
+      <div className="MyCourseText">My Course</div></div>
+      <div className="MyAccountConteiner">
+          <div className="MyAccountLogo"></div>
+          <div className="MyAccountText">My Account</div>
+      </div>
+      <div className="LogoutConteiner">
+          <div className="LogoutLogo"></div>
+          <div className="LogoutText" onClick={press}>Log out</div>
+      </div>
+
+    </div>
+  </div>
+   
+  );
+  function press() {
+    console.log("press")
+  }
+};
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import "./Home.css";
 import { Header } from "../../components/Header";
 import { Menu } from "../../components/Menu";
+import { LeftMenu } from "../../components/LeftMenu/LeftMenu";
 
 export const Home = () => {
   return (
     <div className="Home">
       <Header />
       <Menu />
+      <LeftMenu />
     </div>
   );
 };
